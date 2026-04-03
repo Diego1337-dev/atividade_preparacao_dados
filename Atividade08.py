0c087e539a20c963d4a19c7fe96a264d6e57e22f
Diego_Henrique <dididiego338@gmail.com>
2026-04-03 19:54:07 -0300

    Add files via upload

---
+++ Atividade08.py
@@ -0,0 +1,81 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+df = pd.read_csv('ecommerce_preparados.csv')
+
+#  Remoção de dados nulos/vazios
+print('Tipagem dos dados: \n', df.dtypes)
+print('Quantidade de valores nulos:', df.isnull().sum())
+df['Material'] = df['Material'].fillna('não informado')
+df['Gênero'] = df['Gênero'].fillna('não informado')
+df = df.fillna(0)
+print('Quantidade de valores nulos:', df.isnull().sum().sum())
+
+df['Marca'] = df['Marca'].str.title()
+df['Material'] = df['Material'].str.title()
+df['Temporada'] = df['Temporada'].str.title()
+
+#  Criando um dicionário para padronizar os valores
+mapeamento_temporadas = {
+    'Não Definido': 'Indefinido',
+    'Outono/Inverno': 'Outono-Inverno',
+    'Outono-Inverno': 'Outono-Inverno',
+    'Primavera/Verão': 'Primavera-Verão',
+    'Primavera-Verão': 'Primavera-Verão',
+    'Primavera/Verão/Outono/Inverno': 'Todas as Estações',
+    'Primavera/Verão Outono/Inverno': 'Todas as Estações',
+    'Primavera-Verão Outono-Inverno': 'Todas as Estações',
+    'Primavera-Verão - Outono-Inverno': 'Todas as Estações'
+}
+
+#  Aplicando a substituição
+df['Temporada'] = df['Temporada'].replace(mapeamento_temporadas)
+
+#  Removendo valores irrelevantes como "2021"
+df = df[df['Temporada'] != '2021']
+
+print(df.head(10).to_string())
+print(df.tail(10).to_string())
+
+#  Gráfico de barras
+x = df['Temporada'].value_counts().index
+y = df['Temporada'].value_counts().values
+
+plt.figure(figsize=(10, 6))
+plt.bar(x, y, color='#60aa65')
+plt.title('Temporada')
+plt.xlabel('Vendas por Temporada')
+plt.ylabel('Quantidade')
+plt.xticks(rotation=0)
+plt.show()
+
+#  Gráfico de Pizza
+plt.figure(figsize=(15,8))
+plt.pie(y, labels=x, autopct='%.2f%%', startangle=90)
+plt.title('Distribuição de Vendas por Estação do Ano')
+plt.show()
+
+#  Gráfico Pairplot - Dispersão e Histograma
+sns.pairplot(df[['Qtd_Vendidos_Cod', 'Preço', 'Marca_Cod']])
+plt.show()
+
+#  Gráfico de Densidade - Quantidade de Vendas
+plt.figure(figsize=(10,6))
+sns.kdeplot(df['Qtd_Vendidos_Cod'], fill=True, color='#863e9c')
+plt.title('Densidade de Vendas')
+plt.xlabel('Quantidade de Vendas')
+plt.show()
+
+#  Gráfico de Regressão
+sns.regplot(x='Qtd_Vendidos_Cod', y='N_Avaliações', data=df, color='#278f65', scatter_kws={'alpha': 0.5, 'color': '#34c289'})
+plt.title('Regressão de Quantidade de Vendas por Avaliações')
+plt.xlabel('Quantidade Vendas')
+plt.ylabel('Avaliações')
+plt.show()
+
+#  Mapa de Calor
+corr = df[['Qtd_Vendidos_Cod', 'N_Avaliações']].corr()
+sns.heatmap(corr, annot=True, cmap='coolwarm')
+plt.title('Correlação Quantidade de Vendido e Avaliações')
+plt.show()
